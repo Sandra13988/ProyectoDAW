8073bee65f5d950c3c5b36cecee3f0254d279223
Sandra13988 <sandra13988@gmail.com>
2024-05-15 13:53:09 +0200

    Reubicacion de css menu y modificacion de rutas

--- fuentes/Admin/MenuAdmin.php
+++ fuentes/Admin/MenuAdmin.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="../../assets/css/estiloNoticias.css" />
+    <link rel="stylesheet" href="../../assets/css/estiloMenuAdmin.css" />
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,7 +21,7 @@
 	
 <div id="contenedor">
 <?php include('../plantillas/cabecero.php'); ?>
-    <div class="adminLibros">
+    <div id="cuerpo">
         <h2>MENU ADMINISTRADOR</h2>
         <div class="">
             <h3>ALTA LIBRO</h3>

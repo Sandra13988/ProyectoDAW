901a585ad0a6054a95c97b992b3f90a578356534
Sandra13988 <sandra13988@gmail.com>
2024-05-15 13:37:52 +0200

    Guardado

--- fuentes/Pages/Suscripcion.php
+++ fuentes/Pages/Suscripcion.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Protest+Riot&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="estiloMenus.css">
+        <link rel="stylesheet" href="../../assets/css/estiloMenus.css">
         <title>Suscripciones</title>
     </head>
     <body>

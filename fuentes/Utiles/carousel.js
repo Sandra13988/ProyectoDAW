d2c7647de8ecf0ee81629696b6d9802f5824e540
Sandra13988 <sandra13988@gmail.com>
2024-05-21 10:26:11 +0200

    Obtencion de portadas a traves de json.bin

--- fuentes/Utiles/carousel.js
+++ fuentes/Utiles/carousel.js
@@ -1,33 +1,26 @@
 'use strict';
 
+// Requiere una clave de API y un ID de Bin de JSONbin.io
+const API_KEY = '$2a$10$AIjaA8Tho0hI8s8uxoMEBOfgSlgXj0TVHwaK0uHEPIIUe8zuDBISe';  // Reemplaza con tu clave de API
+const BIN_ID = '664bba2cad19ca34f86c91ba';    // Reemplaza con el ID de tu bin
+
+// Función para obtener los datos actuales del bin
+async function getJsonData() {
+    const response = await fetch(`https://api.jsonbin.io/v3/b/664bba2cad19ca34f86c91ba`, {
+        method: 'GET',
+        headers: {
+            'X-Master-Key': '$2a$10$8Ls7wNx8qPs98jugz8slSeaydaYTVGx6/Ctqlk7FhMuYPNKF4nNNu'
+        }
+    });
+    const data = await response.json();
+    return data.record;
+}
+
 class Carousel {
-  constructor(el) {
+  constructor(el, data) {
     this.el = el;
-    this.carouselOptions = ['previous', 'play', 'next']; //'add' para añadir un libro
-    this.carouselData = [
-      //CARRUSEL HECHO, FALTA AÑADIR LOS ENLACES Y LOS BOTONES DE DESCARGA Y WISHLIST
-      {
-        'id': '1',
-        'src': '../../Grandes/almas.jpg',
-        'url': ''
-      },
-      {
-        'id': '2',
-        'src': '../../Grandes/bodas.jpg',
-      },
-      {
-        'id': '3',
-        'src': '../../Grandes/dulce.jpg',
-      },
-      {
-        'id': '4',
-        'src': '../../Grandes/games.jpg',
-      },
-      {
-        'id': '5',
-        'src': '../../Grandes/harry.jpg',
-      }
-    ];
+    this.carouselOptions = ['previous', 'play', 'next'];
+    this.carouselData = data; // Usa los datos obtenidos de JSONbin
     this.carouselInView = [1, 2, 3, 4, 5];
     this.carouselContainer;
     this.carouselPlayState;
@@ -37,33 +30,25 @@ class Carousel {
     this.setupCarousel();
   }
 
-  // Build carousel html
   setupCarousel() {
     const container = document.createElement('div');
     const controls = document.createElement('div');
 
-    // Add container for carousel items and controls
     this.el.append(container, controls);
     container.className = 'carousel-container';
     controls.className = 'carousel-controls';
 
-    // Take dataset array and append items to container
     this.carouselData.forEach((item, index) => {
       const carouselItem = item.src ? document.createElement('img') : document.createElement('div');
-
       container.append(carouselItem);
-      
-      // Add item attributes
+
       carouselItem.className = `carousel-item carousel-item-${index + 1}`;
       carouselItem.src = item.src;
       carouselItem.setAttribute('loading', 'lazy');
-      // Used to keep track of carousel items, infinite items possible in carousel however min 5 items required
       carouselItem.setAttribute('data-index', `${index + 1}`);
 
-      // Add click event to each image
       carouselItem.addEventListener('click', () => {
-          // Redirect to corresponding URL when clicked
-          window.location.href = item.url || '#'; // Use item.url if available, otherwise default to '#'
+          window.location.href = item.url || '#';
       });
     });
 
@@ -71,33 +56,24 @@ class Carousel {
       const btn = document.createElement('button');
       const axSpan = document.createElement('span');
 
-      // Add accessibilty spans to button
       axSpan.innerText = option;
       axSpan.className = 'ax-hidden';
       btn.append(axSpan);
 
-      // Add button attributes
       btn.className = `carousel-control carousel-control-${option}`;
       btn.setAttribute('data-name', option);
 
-      // Add carousel control options
       controls.append(btn);
     });
 
-    // After rendering carousel to our DOM, setup carousel controls' event listeners
     this.setControls([...controls.children]);
-
-    // Set container property
     this.carouselContainer = container;
-}
-
+  }
 
   setControls(controls) {
     controls.forEach(control => {
       control.onclick = (event) => {
         event.preventDefault();
-
-        // Manage control actions, update our carousel data first then with a callback update our DOM
         this.controlManager(control.dataset.name);
       };
     });
@@ -106,97 +82,55 @@ class Carousel {
   controlManager(control) {
     if (control === 'previous') return this.previous();
     if (control === 'next') return this.next();
-  //  if (control === 'add') return this.add();
     if (control === 'play') return this.play();
-
     return;
   }
 
   previous() {
-    // Update order of items in data array to be shown in carousel
     this.carouselData.unshift(this.carouselData.pop());
-
-    // Push the first item to the end of the array so that the previous item is front and center
     this.carouselInView.push(this.carouselInView.shift());
 
-    // Update the css class for each carousel item in view
     this.carouselInView.forEach((item, index) => {
       this.carouselContainer.children[index].className = `carousel-item carousel-item-${item}`;
     });
 
-    // Using the first 5 items in data array update content of carousel items in view
     this.carouselData.slice(0, 5).forEach((data, index) => {
       document.querySelector(`.carousel-item-${index + 1}`).src = data.src;
     });
   }
 
   next() {
-    // Update order of items in data array to be shown in carousel
     this.carouselData.push(this.carouselData.shift());
-
-    // Take the last item and add it to the beginning of the array so that the next item is front and center
     this.carouselInView.unshift(this.carouselInView.pop());
 
-    // Update the css class for each carousel item in view
     this.carouselInView.forEach((item, index) => {
       this.carouselContainer.children[index].className = `carousel-item carousel-item-${item}`;
     });
 
-    // Using the first 5 items in data array update content of carousel items in view
     this.carouselData.slice(0, 5).forEach((data, index) => {
       document.querySelector(`.carousel-item-${index + 1}`).src = data.src;
     });
   }
-
-  // add() {
-  //   const newItem = {
-  //     'id': '',
-  //     'src': '',
-  //   };
-  //   const lastItem = this.carouselData.length;
-  //   const lastIndex = this.carouselData.findIndex(item => item.id == lastItem);
-    
-  //   // Assign properties for new carousel item
-  //   Object.assign(newItem, {
-  //     id: `${lastItem + 1}`,
-  //     src: `http://fakeimg.pl/300/?text=${lastItem + 1}`
-  //   });
-
-  //   // Then add it to the "last" item in our carouselData
-  //   this.carouselData.splice(lastIndex + 1, 0, newItem);
-
-  //   // Shift carousel to display new item
-  //   this.next();
-  // }
 
   play() {
     const playBtn = document.querySelector('.carousel-control-play');
     const startPlaying = () => this.next();
 
     if (playBtn.classList.contains('playing')) {
-      // Remove class to return to play button state/appearance
       playBtn.classList.remove('playing');
-
-      // Remove setInterval
-      clearInterval(this.carouselPlayState); 
-      this.carouselPlayState = null; 
+      clearInterval(this.carouselPlayState);
+      this.carouselPlayState = null;
     } else {
-      // Add class to change to pause button state/appearance
       playBtn.classList.add('playing');
-
-      // First run initial next method
       this.next();
-
-      // Use play state prop to store interval ID and run next method on a 1.5 second interval
       this.carouselPlayState = setInterval(startPlaying, 3500);
     };
   }
-
 }
 
-// Refers to the carousel root element you want to target, use specific class selectors if using multiple carousels
-const el = document.querySelector('.carousel');
-// Create a new carousel object
-const exampleCarousel = new Carousel(el);
-// Setup carousel and methods
-exampleCarousel.mounted();
+(async () => {
+  const el = document.querySelector('.carousel');
+  const data = await getJsonData(); // Obtener los datos de JSONbin
+  const exampleCarousel = new Carousel(el, data); // Pasar los datos al constructor
+  exampleCarousel.mounted();
+})();

f78723bdf718eb90e13f2c3d8564ea1320ac51cb
Sandra13988 <sandra13988@gmail.com>
2024-05-22 14:00:43 +0200

    Arreglado responsivo tablet

--- fuentes/plantillas/cabecero copy.php
+++ fuentes/plantillas/cabecero copy.php
@@ -74,24 +74,26 @@
         <script src="scriptMenu.js"></script>
     </div>
     <!--Menu en movil-->
-    <div class="menu-container-movil" >
-        <div class="menu-item">
-            <button class="menu-btn"><?php echo $_COOKIE['nombre_usuario'] ?></button>
-            <div class="submenu" id="submenu">
-                <a href="../Pages/Indice.php">Novedades</a>
-                <a href="../Pages/Libros.php">Libros</a>
-                <a href="../Pages/Noticias.php">Noticias</a>
-                <a href="../Pages/Contacto.php">Contacto</a>
-                <a href="../Pages/Perfil.php">Perfil</a>
-                <a href="../Pages/Suscripcion.php">Suscripcion</a>
-                <a href="../Pages/WishList.php">WishList</a>
-                <?php
-                if (comprobarRol()) {
-                    echo '<a href="../Admin/MenuAdmin.php">Panel Administrador</a>';
-                }
-                ?>
+    <div class="contenedorMenuMovil">
+        <div class="menu-container-movil">
+            <div class="menu-item">
+                <button class="menu-btn"><?php echo $_COOKIE['nombre_usuario'] ?></button>
+                <div class="submenu" id="submenu">
+                    <a href="../Pages/Indice.php">Novedades</a>
+                    <a href="../Pages/Libros.php">Libros</a>
+                    <a href="../Pages/Noticias.php">Noticias</a>
+                    <a href="../Pages/Contacto.php">Contacto</a>
+                    <a href="../Pages/Perfil.php">Perfil</a>
+                    <a href="../Pages/Suscripcion.php">Suscripcion</a>
+                    <a href="../Pages/WishList.php">WishList</a>
+                    <?php
+                    if (comprobarRol()) {
+                        echo '<a href="../Admin/MenuAdmin.php">Panel Administrador</a>';
+                    }
+                    ?>
 
-                <a href="../../cerrar.php">Cerrar sesión</a>
+                    <a href="../../cerrar.php">Cerrar sesión</a>
+                </div>
             </div>
         </div>
     </div>

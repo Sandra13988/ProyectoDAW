ed9a98ef11dab92d10555df3d1fca248da5b5db7
Sandra13988 <sandra13988@gmail.com>
2024-05-22 16:23:45 +0200

    Añadido filtros en listado de libros admin

--- fuentes/Admin/listadoLibro.php
+++ fuentes/Admin/listadoLibro.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-	<link rel="stylesheet" href="../../assets/css/estiloListado.css" />
-	<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-	<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Lobster&family=Protest+Riot&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="../../assets/css/estiloMenus.css" />
-	<title>Lista de libros</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="../../assets/css/estiloListado.css" />
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Protest+Riot&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/estiloMenus.css" />
+    <title>Lista de libros</title>
 </head>
 <title>Listado de los artículos</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
@@ -20,65 +20,81 @@
 </head>
 
 <body>
-	<div id="contenedor">
-		<?php include('../plantillas/cabecero.php'); ?>
-		<main id="cuerpo">
-			<?php
+    <div id="contenedor">
+        <?php include('../plantillas/cabecero.php'); ?>
+        <main id="cuerpo">
+            <?php
+            $conexion = conectar();
+            
+            // Consulta para obtener géneros únicos
+            $consultaGeneros = "SELECT DISTINCT genero FROM libros";
+            $resultadoGeneros = mysqli_query($conexion, $consultaGeneros);
+            ?>
+			<div class="formularioFiltro">
+            <form method="GET" action="listadoLibro.php" >
+                <label for="genero">GENERO:</label>
+                <select name="genero" id="genero">
+                    <option value="">Todos</option>
+                    <?php
+                    while ($filaGenero = mysqli_fetch_assoc($resultadoGeneros)) {
+                        $generoOption = $filaGenero['genero'];
+                        echo "<option value='$generoOption'>" . htmlspecialchars($generoOption) . "</option>";
+                    }
+                    ?>
+                </select>
+                <input type="submit" value="Filtrar">
+            </form>
+			</div>
 
-			if (isset($_SERVER["REQUEST_METHOD"]) == "GET" && isset($_GET["pagina"])) {
-				$indice = (int)$_GET["pagina"];
-			} else {
-				$indice = 1;
-			}
+            <?php
+            $genero = isset($_GET['genero']) ? $_GET['genero'] : '';
+            $indice = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 
+            $filtroGenero = $genero ? " WHERE genero='$genero'" : '';
+            $consultaRegistros = "SELECT * FROM libros" . $filtroGenero;
+            $resultadoRegistros = mysqli_query($conexion, $consultaRegistros);
 
-			$conexion = conectar();
-			$consultaRegistros = "SELECT * FROM libros";
-			$resultadoRegistros = mysqli_query($conexion, $consultaRegistros);
+            $numeroDeRegistros = mysqli_num_rows($resultadoRegistros);
+            $numeroDeRegistrosPorPagina = 2;
+            $totalPaginas = ceil($numeroDeRegistros / $numeroDeRegistrosPorPagina);
+            $limite = " LIMIT " . (($indice - 1) * $numeroDeRegistrosPorPagina) . " , " . $numeroDeRegistrosPorPagina;
 
-			$numeroDeRegistros = mysqli_num_rows($resultadoRegistros);
-			$numeroDeRegistrosPorPagina = 2;
-			$totalPaginas = ceil($numeroDeRegistros / $numeroDeRegistrosPorPagina);
-			$limite = " limit " . (($indice - 1) * $numeroDeRegistrosPorPagina) . " , " . $numeroDeRegistrosPorPagina;
+            $consultaLibros = "SELECT * FROM libros" . $filtroGenero . $limite;
+            $resultadoLibros = mysqli_query($conexion, $consultaLibros);
+            $filaLibros = mysqli_fetch_assoc($resultadoLibros);
 
-			$consultaLibros = "SELECT * FROM libros $limite";
-			$resultadoLibros = mysqli_query($conexion, $consultaLibros);
-			$filaLibros = mysqli_fetch_assoc($resultadoLibros);
-			if ($filaLibros) {
-				echo "<div class='posicionDiv'>";
-				echo "<table class='tablaListado'>";
-				echo "<tr>";
-				echo "<th>ISBN</th>";
-				echo "<th>NOMBRE</th>";
-				echo "<th>AUTOR</th>";
-				echo "<th>GENERO</th>";
-				echo "<th class='cuadroPDF'>PDF</th>";
-				echo "</tr>";
+            if ($filaLibros) {
+                echo "<div class='posicionDiv'>";
+                echo "<table class='tablaListado'>";
+                echo "<tr>";
+                echo "<th>ISBN</th>";
+                echo "<th>NOMBRE</th>";
+                echo "<th>AUTOR</th>";
+                echo "<th>GENERO</th>";
+                echo "<th class='cuadroPDF'>PDF</th>";
+                echo "</tr>";
 
-				do {
-					echo "<tr>";
-					echo "<td>" . $filaLibros['isbn'] . "</td>";
-					echo "<td>" . $filaLibros['nombre'] . "</td>";
-					echo "<td>" . $filaLibros['autor'] . "</td>";
-					echo "<td>" . $filaLibros['genero'] . "</td>";
-					echo "<td>" . $filaLibros['pdf'] . "</td>";
-
-
-					echo "</tr>";
-				} while ($filaLibros = mysqli_fetch_assoc($resultadoLibros));
-				echo "</table>";
-				echo "</div>";
-				echo "<div class='botonesPaginacion'>";
-				echo "<a href='listadoLibro.php?pagina=" . (($indice - 1 < 1) ? 1 : $indice - 1) . "'>ATRAS</a>";
-				echo "<a href='fpdf.php'>Descargar PDF</a>";
-				echo "<a href='listadoLibro.php?pagina=" . (($indice + 1 > $totalPaginas) ? $totalPaginas : $indice + 1) . "'>SIGUIENTE</a>";
-				echo "</div>";
-			}
-			?>
-		</main>
-		<?php include("../plantillas/fotter.php"); ?>
-	</div>
+                do {
+                    echo "<tr>";
+                    echo "<td>" . $filaLibros['isbn'] . "</td>";
+                    echo "<td>" . $filaLibros['nombre'] . "</td>";
+                    echo "<td>" . $filaLibros['autor'] . "</td>";
+                    echo "<td>" . $filaLibros['genero'] . "</td>";
+                    echo "<td>" . $filaLibros['pdf'] . "</td>";
+                    echo "</tr>";
+                } while ($filaLibros = mysqli_fetch_assoc($resultadoLibros));
+                echo "</table>";
+                echo "</div>";
+                echo "<div class='botonesPaginacion'>";
+                echo "<a href='listadoLibro.php?pagina=" . (($indice - 1 < 1) ? 1 : $indice - 1) . "&genero=" . $genero . "'>ATRAS</a>";
+                echo "<a href='fpdf.php'>Descargar PDF</a>";
+                echo "<a href='listadoLibro.php?pagina=" . (($indice + 1 > $totalPaginas) ? $totalPaginas : $indice + 1) . "&genero=" . $genero . "'>SIGUIENTE</a>";
+                echo "</div>";
+            }
+            ?>
+        </main>
+        <?php include("../plantillas/fotter.php"); ?>
+    </div>
 </body>
-
 
 </html>
